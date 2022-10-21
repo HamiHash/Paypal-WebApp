39955114ab1251e5eb002512d0264b563c127478
HamiHash <hami.hash@yahoo.com>
2022-10-21 22:32:17 +0330

    Initial commit

---
+++ script.js
@@ -0,0 +1,285 @@
+"use strict";
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+console.log(accounts);
+// Elements
+const labelWelcome = document.querySelector(".welcome");
+const labelDate = document.querySelector(".date");
+const labelBalance = document.querySelector(".balance__value");
+const labelSumIn = document.querySelector(".summary__value--in");
+const labelSumOut = document.querySelector(".summary__value--out");
+const labelSumInterest = document.querySelector(".summary__value--interest");
+const labelTimer = document.querySelector(".timer");
+
+const containerApp = document.querySelector(".app");
+const containerMovements = document.querySelector(".movements");
+
+const btnLogin = document.querySelector(".login__btn");
+const btnTransfer = document.querySelector(".form__btn--transfer");
+const btnLoan = document.querySelector(".form__btn--loan");
+const btnClose = document.querySelector(".form__btn--close");
+const btnSort = document.querySelector(".btn--sort");
+
+const inputLoginUsername = document.querySelector(".login__input--user");
+const inputLoginPin = document.querySelector(".login__input--pin");
+const inputTransferTo = document.querySelector(".form__input--to");
+const inputTransferAmount = document.querySelector(".form__input--amount");
+const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+const inputCloseUsername = document.querySelector(".form__input--user");
+const inputClosePin = document.querySelector(".form__input--pin");
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+
+    const html = `
+    <div class="movements__row">
+     <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+     <div class="movements__value">${mov}</div>
+    </div>
+    `;
+
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+displayMovements(account1.movements);
+
+const calcDisplayBalance = function (mov) {
+  const balance = mov.reduce((acc, mov) => acc + mov);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (acc) {
+  const income = acc.movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${income}`;
+
+  const out = acc.movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}`;
+
+  const itrest = acc.movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * acc.interestRate) / 100)
+    .filter((int) => int >= 1)
+    .reduce((acc, intrest) => acc + intrest, 0);
+  labelSumInterest.textContent = `${itrest}`;
+};
+
+const createUsernames = function (allAccounts) {
+  allAccounts.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
+createUsernames(accounts);
+
+//// Event handler
+
+let currentAccount;
+btnLogin.addEventListener("click", function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    containerApp.style.opacity = 100;
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = "";
+    inputLoginPin.blur();
+    // Disply Movements
+    displayMovements(currentAccount.movements);
+    // Display Balance
+    calcDisplayBalance(currentAccount.movements);
+    // Display Summary
+    calcDisplaySummary(currentAccount);
+  }
+});
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES;
+
+const currencies = new Map([
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+/*
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+//// Slice (will return a new array)
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -2));
+// 2 ways of how to make a copy of the original array:
+console.log(arr.slice());
+console.log([...arr]);
+
+//// Splice (will mutate the original array)
+console.log(arr.splice(2));
+console.log(arr);
+arr.splice(-1);
+console.log(arr);
+// if we give splice 2 parameters ==> (starts from this index, how many to delete)
+const newArr = [2, 5, 6, 4, 7, 8];
+newArr.splice(2, 3);
+console.log(newArr);
+
+//// Reverse (will mutate the original array)
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+
+//// Concat
+const letters = arr.concat(arr2);
+console.log(letters);
+// we can get the same result with spread too:
+console.log([...arr, ...arr2]);
+
+//// Jojn
+console.log(letters.join(' - '));
+
+//// At
+const array = [23, 11, 64];
+console.log(array.at(0));
+// same as:
+console.log(array[0]);
+// but getting the last elemnts is hard with these 2 older ways:
+console.log(array[arr.length - 1]);
+console.log(array.slice(-1)[0]);
+// with At its easier:
+console.log(array.at(-1));
+console.log(array.at(-2));
+// At also works on strings:
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
+
+//// For Each on arryas
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// arrayThatWeWantToLoopOver.forEach(function (1st is Element, 2nd is the index of elemnt, 3rd is the whole array))
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${mov}`);
+  }
+});
+
+
+//// Map method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+// Shorter way to write map method:
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+console.log(movements, movementsUSD);
+// Like foreach, we can give it 3 arguments: (1st is Element, 2nd is the index of elemnt, 3rd is the whole array) "we can omit index or array if we want to: "
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDescriptions);
+
+//// Filter method
+// (1st is Element, 2nd is the index of elemnt, 3rd is the whole array)
+// "we can omit index or array if we want to: "
+// ex)
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(deposits);
+// ex)
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
+
+// Reduce method
+// (1st is Accumulator, 2nd is the elemnt, 3rd index, 4th array) and after function we
+// should declare an initial value. "Important"
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + cur;
+}, 0);
+console.log(balance);
+// ex) how to get Maximum number with reduce
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+});
+console.log(max);
+
+//// Find method
+// It returns the first matching element (note that it doesn't return array but one elemnt)
+// ex)
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+console.log(accounts);
+// ex)
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
+
+*/
